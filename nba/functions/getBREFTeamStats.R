df2ee3740ffbe2002bf85dbe4fbd73ea9a558c17
Alex Bresler <abresler@asbcllc.com>
2014-11-29 14:21:56 -0800

    fixed

--- nba/functions/getBREFTeamStats.R
+++ nba/functions/getBREFTeamStats.R
@@ -2,9 +2,10 @@ getBREFTeamStatTable <- function(season_end = 2015, table_name = 'team', date = 
 	c('rvest','dplyr','pipeR','RCurl') -> packages
 	lapply(packages, library, character.only = T)
 	'http://www.basketball-reference.com/leagues/' -> base
+	(season_end-1) %>>% paste0("-",season_end) -> season
 	'NBA' -> league
-	'#' %>>% paste0(table_name) -> css_page
 	table_name %>>% tolower -> table_name
+	'#' %>>% paste0(table_name) -> css_page
 	table_name %>>% paste('stats', sep = "_") -> table
 	css_page %>>% paste0(" a") -> css_id
 	base %>>% paste0(league,'_',season_end,".html") -> url
@@ -16,10 +17,10 @@ getBREFTeamStatTable <- function(season_end = 2015, table_name = 'team', date = 
 			~t
 		})
 		t[2] %>>% data.frame %>>% tbl_df -> df
-		c('team','wins','losses','win_pct','games_back','pts_per_game','pts_per_game_opp','srs') -> names(df)
+		c('team','wins','losses','win_pct','games_back','pts.g','opp_pts.g','srs') -> names(df)
 		'Eastern' -> df$conference
 		t[3] %>>% data.frame() %>>% tbl_df -> df2
-		c('team','wins','losses','win_pct','games_back','pts_per_game','pts_per_game_opp','srs') -> names(df2)
+		c('team','wins','losses','win_pct','games_back','pts.g','opp_pts.g','srs') -> names(df2)
 		'Western' -> df2$conference
 		rbind_list(df,df2) -> df
 		df$team %>>% (grepl('\\*',.)) -> df$playoff_team
@@ -33,7 +34,7 @@ getBREFTeamStatTable <- function(season_end = 2015, table_name = 'team', date = 
 			(.$team)  %>>% Trim -> df$team
 		df %>>%
 			filter(!is.na(wins)) -> df
-		df$pts_per_game - df$pts_per_game_opp -> df$point_differential
+		df$pts.g - df$opp_pts.g -> df$point_differential
 		pipeline({
 			url
 			html
@@ -47,42 +48,41 @@ getBREFTeamStatTable <- function(season_end = 2015, table_name = 'team', date = 
 		data.frame(season,table_name = 'standings', bref_team_id, df) -> df
 		'http://www.basketball-reference.com' %>>% paste0(team_url_stems) -> df$bref_team_season_url
 	} else{
-	url %>>% ## get table
-		html %>>%
-		html_nodes(css_page) %>>%
-		html_table(header = F) %>>% data.frame() %>>% tbl_df() -> df
-
-	if(df$X.1[1] == 'Rk'){
-		df %>>%
-			filter(X.1 == "Rk") %>>% as.character -> names
-		'Rk' %>>% grep(x = df$X.1) -> row_of_header #find where rank is
-		(row_of_header + 1) %>>% (df[.:nrow(df),]) -> df #skip that row and go to the end
-		names %>>% tolower-> names(df)} else{
+		url %>>% ## get table
+			html %>>%
+			html_nodes(css_page) %>>%
+			html_table(header = F) %>>% data.frame() %>>% tbl_df() -> df
+		if(df$X.1[1] == 'Rk'){
 			df %>>%
 				filter(X.1 == "Rk") %>>% as.character -> names
 			'Rk' %>>% grep(x = df$X.1) -> row_of_header #find where rank is
 			(row_of_header + 1) %>>% (df[.:nrow(df),]) -> df #skip that row and go to the end
-			names %>>% tolower-> names(df)
-		}
-	names(df) %>>% (gsub('\\%|/','\\.',.)) -> names(df)
-	NULL -> df$rk
-	c('team','arena') -> table_name_character
-	df[,!(df %>>% names) %in% table_name_character] %>>% apply(2, function(x) gsub('\\,','',x) %>>%
-																														 	as.numeric(x))  -> df[,!(df %>>% names) %in% table_name_character] #get rid of commas and make numeric
-	df$team %>>% grepl(pattern = '\\*') -> df$playoff_team
-	df$team %>>% (gsub('\\*','',.)) -> df$team
-	df %>>% nrow() -1  -> rows
-	df[1:rows,] -> df
-	(season_end-1) %>>% paste0("-",season_end) -> season
-paste0("//*[(@id = '",table_name,"')]//a") -> xpath
-	##Grab Team Ids
-	url %>>% ## get table
-		html %>>%
-		xpathSApply(xpath,xmlAttrs) %>>%
-		unlist %>>% as.character -> stems
-	stems %>>% (gsub('\\/|.html|teams','',.)) %>>%
-		(gsub(season_end,'',.)) -> bref_team_id
-	data.frame(season,table_name = table, bref_team_id, df) -> df
+			names %>>% tolower-> names(df)} else{
+				df %>>%
+					filter(X.1 == "Rk") %>>% as.character -> names
+				'Rk' %>>% grep(x = df$X.1) -> row_of_header #find where rank is
+				(row_of_header + 1) %>>% (df[.:nrow(df),]) -> df #skip that row and go to the end
+				names %>>% tolower-> names(df)
+			}
+		names(df) %>>% (gsub('\\%|/','\\.',.)) -> names(df)
+		NULL -> df$rk
+		c('team','arena') -> table_name_character
+		df[,!(df %>>% names) %in% table_name_character] %>>% apply(2, function(x) gsub('\\,','',x) %>>%
+																															 	as.numeric(x))  -> df[,!(df %>>% names) %in% table_name_character] #get rid of commas and make numeric
+		df$team %>>% grepl(pattern = '\\*') -> df$playoff_team
+		df$team %>>% (gsub('\\*','',.)) -> df$team
+		df %>>% nrow() -1  -> rows
+		df[1:rows,] -> df
+		paste0("//*[(@id = '",table_name,"')]//a") -> xpath
+		##Grab Team Ids
+		url %>>% ## get table
+			html %>>%
+			xpathSApply(xpath,xmlAttrs) %>>%
+			unlist %>>% as.character -> stems
+		stems %>>% (gsub('\\/|.html|teams','',.)) %>>%
+			(gsub(season_end,'',.)) -> bref_team_id
+		data.frame(season,table_name = table, bref_team_id, df) -> df
+		'http://www.basketball-reference.com' %>>% paste0(stems) -> df$bref_team_season_url
 	}
 	if(date == T){
 		Sys.time() -> df$scrape_time
