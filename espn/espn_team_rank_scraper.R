9b1564f21021b08e07ba9d86bf7a7683127ca403
Alex Bresler <abresler@asbcllc.com>
2014-12-03 18:45:41 -0500

    data

---
+++ espn/espn_team_rank_scraper.R
@@ -0,0 +1,28 @@
+library(rvest)
+
+'http://espn.go.com/sportsnation/teamrankings#table' %>>%
+	html %>>%
+	html_table() %>>% data.frame %>>% tbl_df -> data
+
+names(data) %<>% tolower()
+c('rank','team') -> names(data)[1:2]
+data$rank  %>>% (gsub('\\.','',.)) %>>% as.numeric -> data$rank
+data[!is.na(data$rank),] -> data
+2014 -> data$year
+2003:2013 -> years
+'http://espn.go.com/sportsnation/teamrankings/_/year/' %>>% paste0(years) -> urls
+data.frame(year = years, url = urls) -> df_urls
+urls[2] -> u
+data.frame() %>>% tbl_df -> all_df
+for(u in urls){
+	u %>>% html %>>%
+		html_table() %>>% data.frame %>>% tbl_df -> df
+	names(df) %<>% tolower()
+	c('rank','team') -> names(df)[1:2]
+	df$rank  %>>% (gsub('\\.','',.)) %>>% as.numeric -> df$rank
+	df[!is.na(df$rank),] -> df
+	df_urls[df_urls$url == u,'year'] -> df$year
+	all_df %>>% rbind_list(df) -> all_df
+}
+all_df %>>% rbind_list(data) -> all_df
+data %>>% write.csv('Desktop/webpage/abresler.github.io/data/espn/team_rankings.csv', row.names = F)
